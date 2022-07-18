efa0a833fa84222fd03b35455feb543dca8e2d53
iys <iysrmt@qq.com>
2022-07-18 17:09:25 +0800

    docs: 完善注释

--- src/main/java/per/iys/crm/settings/service/UserService.java
+++ src/main/java/per/iys/crm/settings/service/UserService.java
@@ -6,7 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface UserService {
+    /**
+     * 登陆
+     * @param map
+     * @return
+     */
     User queryUserByLoginActAndLoginPwd(Map<String, Object> map);
 
+    /**
+     * 查询所有用户
+     * @return
+     */
     List<User> queryAllUsers();
 }
